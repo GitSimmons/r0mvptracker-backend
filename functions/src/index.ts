793acec34e9c0c4b44a8b4aeb8d90129278e055d
Ben Simmons <ben.ryan.simmons@gmail.com>
2020-07-06 14:42:50 -0400

    config puppeteer to whitelist only the files we need

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@
 import * as chromium from "chrome-aws-lambda";
 
 export const MVPTable = async () => {
-  // if stage is dev, use local chromium
+  // If stage is dev, use local chromium
   const executablePath =
     process.env.DEV_ANDROID == "true"
       ? "/usr/bin/chromium-browser"
@@ -14,6 +14,19 @@ export const MVPTable = async () => {
     defaultViewport: chromium.defaultViewport,
   });
   const page = await browser.newPage();
+  // In order to minimize server load, we use a whitelist
+  // 1. Intercept network requests.
+  await page.setRequestInterception(true);
+  page.on("request", (req) => {
+    // 2. Ignore requests for resources that don't produce DOM
+    // (images, stylesheets, media).
+    const whitelist = ["document", "script", "xhr", "fetch"];
+    if (!whitelist.includes(req.resourceType())) {
+      return req.abort();
+    }
+    // 3. Pass through all other requests.
+    req.continue();
+  });
   const url = "http://www.ragna0.com/ranking/mvp/";
   await page.goto(url);
   const MVPs = ["somevalues"];
