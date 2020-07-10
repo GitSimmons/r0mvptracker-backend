ce01ab50f9e3abc067b998fbb3acd353a802e811
Ben Simmons <ben.ryan.simmons@gmail.com>
2020-07-10 09:55:18 -0400

    feat: add cloud function!

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,33 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import { scrapeLatestKills } from "./scrapeLatestKills";
+import { MVP } from "./types";
 
-admin.initializeApp();
+if (admin.apps.length === 0) {
+  admin.initializeApp();
+}
+let db = admin.firestore();
 
 exports.addLatestKills = functions.pubsub
   .schedule("every 5 minutes")
-  .onRun(() => {
-    console.log("this runs every 5 minutes");
+  .onRun(async () => {
+    // 1. scrape latest kills
+    const MVPs: MVP[] = await scrapeLatestKills();
+    /* TODO: see if pruning excess writes saves time
+     * 1. get .doc(lastUpdated)
+     * 2. only write MVPs if lastUpdated > .doc(lastUpdated)
+     * */
+    // 2. for each kill, merge new whoKilled && lastKilled data
+    const dbUpdates = MVPs.map(
+      async (mvp) =>
+        await db.collection("mvp").doc(mvp.name).set(mvp, { merge: true })
+    );
+    await Promise.all(dbUpdates);
+    // 3. update the lastUpdated
+    await db
+      .collection("misc")
+      .doc("lastUpdated")
+      .set({ lastUpdated: new Date() });
+    // Cloud fns are asynchronous and should return null, an object or a promise
+    return null;
   });
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
